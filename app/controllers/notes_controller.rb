1661ab820273e457b277482bf08798e8fa461403
JustinBrown <brown.jt@gmail.com>
2014-11-24 16:17:07 -0700

    Rudimentary form for new notes.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,4 +2,16 @@ class NotesController < ApplicationController
   def index
     @notes = Note.all
   end
+
+  def new
+    @note = Note.new
+  end
+
+  def create
+    @note = Note.new
+    @note.text = params[:note][:text]
+    @note.save
+    redirect_to notes_path
+  end
+
 end
